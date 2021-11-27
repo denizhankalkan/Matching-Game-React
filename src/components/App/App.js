d97f14f1d8deff0de0d956297c54d792321d9ce7
denizhankalkan <denizhn1307@gmail.com>
2021-11-27 23:22:14 +0300

    method-fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import {createTiles} from '../../misc/utils';
+import {createTiles, indexOfSelected} from '../../misc/utils';
 
 import './App.css';
 
@@ -26,6 +26,27 @@ startGame = (numTiles) => {
     toBeCleared: null,
     tiles: createTiles(state.numTiles)
   }))
+}
+
+HandleTileClicked = (id, color) => {
+   this.state((state) => {
+     const tiles = state.tiles
+     let toBeCleared = state.toBeCleared  
+     const selectedTileIndex = indexOfSelected(tiles, id, color)
+     let previousTileIndex = state.previousTileIndex
+
+     if(previousTileIndex !== null){
+       const previousTile = tiles[previousTileIndex]
+       const selectedTile = tiles[selectedTileIndex]
+     } else {
+       previousTileIndex = selectedTileIndex
+     }
+
+     return {toBeCleared, tiles, previousTileIndex}
+
+
+    }
+   ) 
 }
 
 
